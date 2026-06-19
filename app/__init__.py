f53cba4bd0a959a5bcca751edcad130443122db8
avi1967 <184269098+avi1967@users.noreply.github.com>
2026-06-19 17:20:02 +0530

    Initialize Flask application structure

--- app/__init__.py
+++ app/__init__.py
@@ -5,15 +5,17 @@ from flask_login import LoginManager
 db = SQLAlchemy()
 login_manager = LoginManager()
 
+import os
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+    app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'your_secret_key_123_abc')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///database.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'login'
+    login_manager.login_view = 'main.login'
 
     from .routes import main
     app.register_blueprint(main)
--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 from app import create_app, db
 from app.models import User, Question, Answer
+import os
 
 app = create_app()
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, port=port)
